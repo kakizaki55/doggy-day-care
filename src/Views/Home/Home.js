e76eed17411633cc30a2a586e67872676537dd2e
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-03 15:08:27 -0800

    forgot to do an initail commit. started working on the home page view componnet

---
+++ src/Views/Home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [dogs, setDogs] = useState([]);
+
+  return <div>this is the home page</div>;
+}
