3c4aa50cead9235085031c290701865bfbf8bf3a
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-04 17:38:26 -0800

    got al test to work

--- src/Views/DogDetails/DogDetails.test.js
+++ src/Views/DogDetails/DogDetails.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import Dogdetails from './Dogdetails';
+import DogDetails from './Dogdetails';
+import { MemoryRouter, Route } from 'react-router-dom';
 
-it.skip('span shot test for the home View container', async () => {
-  const { container } = render(<Dogdetails />);
-  await screen.findByText('home');
+it('span shot test for the home View container', async () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/dog/11']}>
+      <Route exact path="/dog/:id" component={DogDetails}></Route>
+    </MemoryRouter>
+  );
+  await screen.findByText('Meet Happy !');
   expect(container).toMatchSnapshot();
 });
