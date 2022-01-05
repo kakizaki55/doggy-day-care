1478c1c18b14cd0f5f01ec14fc7957858bb7d987
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-05 15:34:33 -0800

    got the add dog call to work fine next im looking in to creating a delete button

--- src/Views/AddDog/AddDog.js
+++ src/Views/AddDog/AddDog.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useState } from 'react/cjs/react.development';
 import UpdateDogFrom from '../../components/UpdateDogFrom/UpdateDogForm';
+import { addDogToDataBase } from '../../services/updateDog';
+
 export default function AddDog() {
   const [dog, setDog] = useState({});
   const [loading, setLoading] = useState(true);
 
-  const handelSubmit = (e) => {
+  const updateDog = (key, value) => {
+    dog[key] = value;
+    setDog({ ...dog });
+  };
+  const handelSubmit = async (e) => {
     e.preventDefault();
-    console.log('admin button pressed');
+    await addDogToDataBase(dog);
   };
 
   return (
     <>
-      <UpdateDogFrom dog={''} handelSubmit={handelSubmit} />
+      <UpdateDogFrom dog={''} updateDog={updateDog} handelSubmit={handelSubmit} />
     </>
   );
 }
--- src/services/updateDog.js
+++ src/services/updateDog.js
@@ -4,3 +4,8 @@ export async function updateDogInDataBase(dog) {
   const data = await client.from('dogs').update(dog).eq('id', dog.id);
   return checkError(data);
 }
+
+export async function addDogToDataBase(dog) {
+  const data = await client.from('dogs').insert(dog);
+  return checkError(data);
+}
