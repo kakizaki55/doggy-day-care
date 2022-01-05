2dad2a84c1f8779f245a717361bb6df1ea27da25
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-05 15:08:05 -0800

    got thehandle submit button to do the console log

--- src/Views/AddDog/AddDog.js
+++ src/Views/AddDog/AddDog.js
@@ -1,5 +1,18 @@
 import React from 'react';
-
+import { useState } from 'react/cjs/react.development';
+import UpdateDogFrom from '../../components/UpdateDogFrom/UpdateDogForm';
 export default function AddDog() {
-  return <div>add dog page</div>;
+  const [dog, setDog] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    console.log('admin button pressed');
+  };
+
+  return (
+    <>
+      <UpdateDogFrom dog={''} handelSubmit={handelSubmit} />
+    </>
+  );
 }
