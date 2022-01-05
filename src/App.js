cff8e5038019b1c96a2d6922b9f2988697e43e92
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-05 15:56:50 -0800

    just cleaning up a few imports from react router dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Home from './Views/Home/Home.js';
 import DogDetails from './Views/DogDetails/Dogdetails';
 import UpdateDog from './Views/UpdateDog/UpdateDog';
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom';
 import AddDog from './Views/AddDog/AddDog';
 
 function App() {
--- src/components/HomeDogCard/HomeDogCard.test.js
+++ src/components/HomeDogCard/HomeDogCard.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter } from 'react-router-dom';
 
 import HomeDogCard from './HomeDogCard';
 test('making sure the home dog card renders correctly', async () => {
